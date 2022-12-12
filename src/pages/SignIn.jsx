d32249956671c965ffc896def93218081e5c01c0
Ravishankar Rajagopalan <rravi108@gmail.com>
2022-12-12 16:27:16 +0530

    complete the sign in functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { useState } from 'react';
 import {AiFillEyeInvisible, AiFillEye} from 'react-icons/ai'
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { signInWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { toast } from 'react-toastify';
 
 export default function SignIn() {
+    const navigate = useNavigate();
     const [showPassword, setShowPassword] = useState(false);
 
     const [formData, setFormData] = useState({
@@ -20,6 +23,20 @@ export default function SignIn() {
         }));
     }
 
+    async function onSubmit(e) {
+        e.preventDefault();
+        try { 
+            const auth = getAuth();
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            if (userCredential.user) {
+                navigate("/")
+            }
+
+        } catch(error) {
+            toast.error("Bad user credentials");
+        }
+    }
+
 
   return (
     <section>
@@ -30,8 +47,8 @@ export default function SignIn() {
 
             </div>
             <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-                <form>
-                    <input type="text" type="email" id="email" value={email} onChange={onChange} placeholder="Email address" className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded  transition ease-in-out" />
+                <form onSubmit={onSubmit}>
+                    <input type="email" id="email" value={email} onChange={onChange} placeholder="Email address" className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded  transition ease-in-out" />
                     <div className='relative mb-6'>
                     <input type={showPassword ? "text": "password"} id="password" value={password} onChange={onChange} placeholder="Password" className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded  transition ease-in-out" />
                     {showPassword ? 
