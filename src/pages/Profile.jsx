a4815fe882ee7a2c154a719818d121bb7bf96cfd
Ravishankar Rajagopalan <rravi108@gmail.com>
2022-12-31 20:09:14 +0530

    add delete and edit functionality to the my listing section

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import {getAuth, updateProfile} from "firebase/auth"
-import { collection, doc, getDocs, orderBy, query, updateDoc, where } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, orderBy, query, updateDoc, where } from "firebase/firestore";
 import React, { useState } from 'react'
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -71,6 +71,21 @@ export default function Profile() {
     fetchUserListings();
   },[auth.currentUser.uid])
 
+  async function onDelete(listingID) {
+    if(window.confirm("Are you sure you want to delete?")) {
+      await deleteDoc(doc(db, "listings", listingID))
+      const updatedListings = listings.filter(
+        (listing)=> listing.id !== listingID
+      );
+      setListings(updatedListings);
+      toast.success("Successfully deleted the listing");
+    }
+    
+  }
+
+  function onEdit(listingID) {
+    navigate(`/edit-listing/${listingID}`)
+  }
   return (
     <>
       <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -107,9 +122,9 @@ export default function Profile() {
         {!loading && listings.length > 0 && (
           <>
           <h2 className="text-2xl text-center font-semibold mb-6">My Listings</h2>
-          <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6">
+          <ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6">
             {listings.map((listing)=>(
-              <ListingItem key={listing.id} id={listing.id} listing={listing.data}/>
+              <ListingItem key={listing.id} id={listing.id} listing={listing.data} onDelete={()=>onDelete(listing.id)} onEdit={()=>onEdit(listing.id)}/>
             ))}
           </ul>
           </>
